c6fece241aea82f0f8fa91a2952db99b96248b8a
Fredrik Andersson <fredrik.andersson1@chasacademy.se>
2024-11-01 12:01:23 +0100

    function för att ta bort tasks med en lista på tasksId

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function showMenu() {
-    prompt("Meny");
+function removeTask() {
+  let showTaskId = "Alla uppgifter:\n\n";
+  tasks.forEach((task) => {
+    showTaskId += `${task.id}: ${task.description} ${
+      task.done ? "KLAR" : "INTE KLAR"
+    }\n`;
+  });
+
+  let taskId = prompt(`${showTaskId} Ange ID för uppgiften du vill ta bort:`);
+  let index = tasks.findIndex((task) => task.id == taskId);
+
+  if (index !== -1) {
+    // Check if task exists
+    tasks.splice(index, 1); // Remove the task
+    console.log(`Uppgiften med ID ${taskId} har tagits bort.`);
+    alert(`Uppgiften med ID ${taskId} har tagits bort.`);
+  } else {
+    console.log("Uppgift hittades inte.");
+    alert("Uppgift hittades inte.");
+  }
+  console.log(tasks);
 }

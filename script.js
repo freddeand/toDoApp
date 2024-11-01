3f1790beaa3cbd885cc3dce6f44f522443227387
Benjamin Stenlund <benjamin.stenlund@chasacademy.se>
2024-11-01 10:22:52 +0100

    showTasks()
    Show all tasks feature.

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function showMenu() {
     prompt("Meny");
 }
+
+
+function showTasks() {
+    // Examples until addNew() is done.
+    let tasks = [
+        {
+            id: 1,
+            text: "Exempeluppgift A.",
+            done: false,
+        },
+        {
+            id: 2,
+            text: "Exempeluppgift B.",
+            done: true,
+        },
+    ]
+
+
+    let allTasksText = "Alla uppgifter:\n\n";
+    tasks.forEach(task => {
+        allTasksText += `${task.id}: ${task.text} ${task.done ? "KLAR": "INTE KLAR"}\n`;
+    })
+
+    alert(allTasksText);
+}

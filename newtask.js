5b64bec2f25599fbc98dd8052d106a6ef319a27e
Fredrik Andersson <fredrik.andersson1@chasacademy.se>
2024-11-08 14:15:22 +0100

    Update newtask.js

--- newtask.js
+++ newtask.js
@@ -54,8 +54,8 @@ function renderTask(task) {
   removeBtn.innerText = "Ta bort uppgift";
   removeBtn.style.margin = "10px";
   removeBtn.addEventListener("click", function () {
-    let removeYes = document.createElement("button");
-    let removeNo = document.createElement("button");
+    let removeYes = document.createElement("button"); // knapp för att tabort om du trycker på tabort uppgift.
+    let removeNo = document.createElement("button"); // knapp för att återgå till uppgiften.
     removeYes.innerText = "ja";
     removeNo.innerText = "nej";
     addPara.appendChild(removeYes);
@@ -76,9 +76,6 @@ function renderTask(task) {
       addPara.removeChild(removeYes);
       addPara.removeChild(removeNo);
     });
-    // taskContainer.removeChild(addPara);
-    // tasks = tasks.filter((t) => t.id !== task.id);
-    // saveTasks(); // Uppdatera localStorage efter borttagning
   });
 
   // "Färdig markera"-knapp
